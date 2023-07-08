b4d7286c56c7d9d46747032a25e79617029bf7a4
Ramin Kuraishy <ramin.kuraishy@gmail.com>
2023-07-09 02:45:36 +0300

    add a few function and small refuctoring

--- nasa/src/components/List/list.js
+++ nasa/src/components/List/list.js
@@ -3,70 +3,114 @@ import "./list.scss";
 
 const apiKey = process.env.REACT_APP_NASA_KEY;
 const List = props => {
-  const [neo, setNeo] = useState(null);
+  const [aggregatedData, setAggregatedData] = useState([]);
   useEffect(() => {
-    fetchNeo();
-    console.log(neo);
+    const startDate = new Date();
+    startDate.setDate(1);
 
-    async function fetchNeo() {
-      const res = await fetch(
-        `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-07-01&end_date=2023-07-07&api_key=${apiKey}`
-      );
-      const data = await res.json();
-      setNeo(data);
+    const endDate = new Date();
+    if (endDate.getDate() > 7) {
+      endDate.setDate(7);
     }
-    return () => {
-    };
-    // const neoArr = neo.near_earth_objects
-  }, []);
 
-  if (!neo) return <div>there is no data</div>;
-  if (
-    neo.near_earth_objects["2023-07-07"][0].is_potentially_hazardous_asteroid
-  ) {
-    neo.near_earth_objects["2023-07-07"][0].is_potentially_hazardous_asteroid =
-      "Potentially harazardous asteroid!!!";
+    fetchNeo(formatDate(startDate), formatDate(endDate), apiKey);
+    // const interval = setInterval(() => {
+    //   fetchNeo(formatDate(startDate), formatDate(endDate), apiKey);
+    // }, 2000);
+
+    // return () => {
+    //   clearInterval(interval);
+    // };
+  }, []);
+  function formatDate(date) {
+    var yyyy = date.getFullYear();
+    var mm = date.getMonth() + 1;
+    if (mm < 10) mm = "0" + mm;
+    var dd = date.getDate();
+    if (dd < 10) dd = "0" + dd;
+    return `${yyyy}-${mm}-${dd}`;
   }
+  function getAggregatedData(neos) {
+    let diameterMax = 0;
+    let hazardousAsteroidCount = 0;
+    let closestNeo = null;
+    let fastestNeo = 0;
+    for (let neo of neos) {
+      if (
+        diameterMax < neo.estimated_diameter.kilometers.estimated_diameter_max
+      ) {
+        diameterMax = neo.estimated_diameter.kilometers.estimated_diameter_max;
+      }
+      if (neo.is_potentially_hazardous_asteroid) {
+        hazardousAsteroidCount++;
+      }
+      if (
+        closestNeo === null ||
+        closestNeo > neo.close_approach_data[0].miss_distance.kilometers
+      ) {
+        closestNeo = neo.close_approach_data[0].miss_distance.kilometers;
+      }
+      if (
+        fastestNeo <
+        neo.close_approach_data[0].relative_velocity.kilometers_per_hour
+      ) {
+        fastestNeo =
+          neo.close_approach_data[0].relative_velocity.kilometers_per_hour;
+      }
+    }
+
+    return {
+      diameterMax,
+      hazardousAsteroidCount,
+      closestNeo,
+      fastestNeo,
+    };
+  }
+
+  async function fetchNeo(startDate, endDate, apiKey) {
+    const res = await fetch(
+      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`
+    );
+    const data = await res.json();
+    const availableDates = Object.keys(data.near_earth_objects).sort();
+    let currentDateIndex = 0;
+    const dataPerDay = [];
+    function getNextDay() {
+      const aggregated = getAggregatedData(
+        data.near_earth_objects[availableDates[currentDateIndex]]
+      );
+      if (currentDateIndex + 1 === availableDates.length) {
+        currentDateIndex = 0;
+      } else {
+        currentDateIndex++;
+      }
+      return {
+        ...{ date: availableDates[currentDateIndex] },
+        ...aggregated,
+      };
+    }
+    for (let i = 0; i < 6; i++) {
+      dataPerDay.push(getNextDay());
+    }
+    setAggregatedData(dataPerDay);
+  }
+
+  if (!aggregatedData) return <div>There is no items</div>;
+
   return (
     <div className="card_wrapper">
       <ul className="page-list">
-        {/* {neoArr.map(item => {
-        return (
-          <li className="page-list-item"> 
-        <h3>{item.name}</h3>
-        </li>
-        )
-      })} */}
-        <li className="page-list-item">
-          <h3>{neo.near_earth_objects["2023-07-07"][0].name}</h3>
-          <p>
-            diameter:{" "}
-            {
-              neo.near_earth_objects["2023-07-07"][0].estimated_diameter
-                .kilometers.estimated_diameter_max
-            }
-          </p>
-          <p>
-            {
-              neo.near_earth_objects["2023-07-07"][0]
-                .is_potentially_hazardous_asteroid
-            }
-          </p>
-          <p>
-            km:{" "}
-            {
-              neo.near_earth_objects["2023-07-07"][0].close_approach_data[0]
-                .miss_distance.kilometers
-            }
-          </p>
-          <p>
-            km per hour:{" "}
-            {
-              neo.near_earth_objects["2023-07-07"][0].close_approach_data[0]
-                .relative_velocity.kilometers_per_hour
-            }
-          </p>
-        </li>
+        {aggregatedData.map((item, key) => {
+          return (
+            <li key={key} className="page-list-item">
+              <h3>{item.date}</h3>
+              <p>Diameter: {item.diameterMax}</p>
+              <p>Potentially hazardous NEO: {item.hazardousAsteroidCount}</p>
+              <p>Closest NEO: {item.closestNeo}</p>
+              <p>Fastest NEO: {item.fastestNeo}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
